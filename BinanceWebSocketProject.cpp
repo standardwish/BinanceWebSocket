a317dd446f57529581587793957213734a044c1e
Jun Seong Pyo <standardstar@m365.hanyang.ac.kr>
2023-12-19 20:11:11 +0900

    주석 달았당

--- BinanceWebSocketProject.cpp
+++ BinanceWebSocketProject.cpp
@@ -5,8 +5,8 @@
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/ssl/stream.hpp>
-#include <sstream>  // std::istringstream
-#include <iomanip>  // std::setprecision
+#include <sstream>  
+#include <iomanip>  
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
@@ -15,6 +15,7 @@
 #include <nlohmann/json.hpp>
 #include <conio.h>
 
+// Namespace aliases for Boost.Beast, Boost.Asio, and JSON library
 namespace beast = boost::beast;
 namespace http = beast::http;
 namespace websocket = beast::websocket;
@@ -23,27 +24,19 @@ namespace ssl = boost::asio::ssl;
 using tcp = boost::asio::ip::tcp;
 using json = nlohmann::json;
 
-int Exit() {
-    if (_kbhit()) {
-        int key1 = _getch();
-        std::cout << key1 << std::endl;
-
-        int key2 = _getch();
-        std::cout << key2 << std::endl;
-    }
-
-    return -1;
-}
-
+// WebSocketClient class for managing WebSocket connections
 class WebSocketClient {
 public:
     WebSocketClient(net::io_context& ioc, ssl::context& ctx)
         : resolver_(ioc), ws_(ioc, ctx) {}
+
+    // Method to establish a WebSocket connection
     void connect(const std::string& host, const std::string& port, const std::string& target) {
         auto const results = resolver_.resolve(host, port);
         net::connect(ws_.next_layer().next_layer(), results.begin(), results.end());
         ws_.next_layer().handshake(ssl::stream_base::client);
 
+        // Set WebSocket options and perform handshake
         ws_.set_option(websocket::stream_base::decorator(
             [](websocket::request_type& req)
             {
@@ -54,15 +47,18 @@ public:
         ws_.handshake(host, target);
         std::cout << "CONNECTION SUCCESS" << std::endl;
     }
-        
+
+    // Method to send a message over the WebSocket connection
     void send(const std::string& message) {
         ws_.write(net::buffer(message));
     }
 
+    // Method to receive data from the WebSocket connection
     void receive() {
         ws_.read(buffer_);
     }
 
+    // Method to process and write received data to a file
     void write() {
         std::ofstream file("stream-data-binance.txt", std::ios_base::app);
         if (file.is_open()) {
@@ -87,7 +83,7 @@ public:
                     << "byte position of error: " << e.byte << std::endl;
             }
             buffer_.clear();
-            
+
         }
         else {
             std::cout << "File Error" << std::endl;
@@ -95,6 +91,7 @@ public:
         file.close();
     }
 
+    // Method to close the WebSocket connection
     void close() {
         ws_.close(websocket::close_code::normal);
         std::cout << "CONNECTION CLOSE" << std::endl;
@@ -106,23 +103,28 @@ private:
     websocket::stream<beast::ssl_stream<tcp::socket>> ws_;
 };
 
+// BinanceWebSocketClient class for managing Binance-specific WebSocket functionality
 class BinanceWebSocketClient {
 public:
     BinanceWebSocketClient(net::io_context& ioc, ssl::context& ctx, const std::string& host, const std::string& port, const std::string& param)
         : wsClient(ioc, ctx), host_(host), port_(port), param_(param), target_("/ws/" + param) {}
 
+    // Method to run the WebSocket client
     void run() {
         try {
             wsClient.connect(host_, port_, target_);
 
+            // Subscribe to Binance WebSocket channels
             const std::string subscribeMessage = R"({"method": "SUBSCRIBE", "params": ["btcusdt@depth5", "btcusdt@trade"], "id": 1})";
             wsClient.send(subscribeMessage);
-            Exit();
+
+            // Main loop for receiving and processing WebSocket data
             while (true) {
                 wsClient.receive();
                 wsClient.write();
             }
 
+            // Close the WebSocket connection (unreachable in this code because of the infinite loop)
             wsClient.close();
         }
         catch (std::exception const& e) {
@@ -138,16 +140,18 @@ private:
     std::string target_;
 };
 
+// Main function
 int main()
 {
     const std::string host = "stream.binance.com";
     const std::string port = "443";
     const std::string param = "btcusdt";
 
+    // Create I/O context and SSL context for the WebSocket client
     net::io_context ioc;
     ssl::context ctx{ ssl::context::tlsv12_client };
 
+    // Create and run the BinanceWebSocketClient
     BinanceWebSocketClient client(ioc, ctx, host, port, param);
     client.run();
-
 }
